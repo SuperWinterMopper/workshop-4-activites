bc89ea1dc25093cdee4aa6d518942a4435a3d1af
Amy Liao <amzhliao@gmail.com>
2024-10-24 19:37:16 -0500

    styling

--- src/App.js
+++ src/App.js
@@ -8,18 +8,19 @@ function App() {
     <>
       <h1>Todo List App</h1>
       <h2>My tasks for today</h2>
+      <button>Dark mode</button>
       <ul>
-        <div>
+        <div style={{backgroundColor: 'lightgrey', width: '500px', height: '100px'}}>
           <h3>Task 1</h3>
           <p>Task 1 description</p>
           <p>task 1 deadline</p>
         </div>
-        <div>
+        <div style={{backgroundColor: 'lightgrey', width: '500px', height: '100px'}}>
           <h3>Task 2</h3>
           <p>Task 2 description</p>
           <p>task 2 deadline</p>
         </div>
-        <div>
+        <div style={{backgroundColor: 'lightgrey', width: '500px', height: '100px'}}>
           <h3>Task 3</h3>
           <p>Task 3 description</p>
           <p>task 3 deadline</p>
