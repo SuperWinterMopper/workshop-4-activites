7b1b1464fae5854e4c9c5613ada148f309c1dd53
Amy Liao <amzhliao@gmail.com>
2024-10-24 20:35:13 -0500

    track whats done

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { useState, useEffect } from 'react';
 import './App.css';
 
 function Item(props) {
+  const color = props.done ? 'lightgreen' : 'lightpink';
+
   return (
-    <div style={{backgroundColor: 'lightpink', width: '500px', height: '100px'}}>
+    <div style={{backgroundColor: color, width: '500px', height: '100px'}} onClick={props.handleDone}>
       <h3>{props.title}</h3>
       <p>{props.description}</p>
       <p>{props.deadline}</p>
@@ -18,8 +20,36 @@ function Title() {
   )
 }
 
+const TASKS = [
+  {
+    title: 'laundry',
+    description: 'wash clothes',
+    deadline: 'tomorrow',
+    done: false,
+  },
+  {
+    title: 'dishes',
+    description: 'pots and pans',
+    deadline: 'oct 30',
+    done: false,
+  },
+  {
+    title: 'taxes',
+    description: 'dont get audited',
+    deadline: 'nov 1',
+    done: false,
+  }
+]
+
 function App() {
   const [mode, setMode] = useState('light');
+  const [tasks, setTasks] = useState(TASKS);
+
+  const handleDone = (index) => {
+    const newTasks = [...tasks];
+    newTasks[index].done = !newTasks[index].done;
+    setTasks(newTasks);
+  }
 
   const switchMode = () => {
     if (mode === 'light') {
@@ -42,9 +72,11 @@ function App() {
       <h2>My tasks for today</h2>
       <button onClick={switchMode}>Dark mode</button>
       <ul>
-        <Item title="laundry" description="wash clothes" deadline="tomorrow" />
-        <Item title="dishes" description="pots and pans" deadline="oct 30" />
-        <Item title="taxes" description="dont get audited" deadline="nov 1" />
+        {tasks.map((task, index) => {
+          return (
+            <Item title={task.title} description={task.description} deadline={task.deadline} done={task.done} handleDone={() => handleDone(index)}/>
+          )
+        })}
       </ul>
     </div>
   );
